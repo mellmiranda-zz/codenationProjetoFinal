9dd2e2f33d52289ffdbdf2584364e175bb458ebc
NayamaRosa <falecom@nayama.com.br>
2020-06-18 14:09:16 -0300

    adição do productInfo component html e scss

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -3,6 +3,7 @@ import React from 'react'
 import './Search.scss'
 
 import Banner from 'components/Banner'
+import ProductInfo from 'components/ProductInfo'
 
 const Search = (props) => {
   return (
@@ -16,20 +17,13 @@ const Search = (props) => {
             classNameDiscount="banner__product--search-discount"
             percent={props.discount_percentage}
           />
-          <div className={`product-info__price-group ${props.classNamePriceGroup}`}>
-            <h4>{props.productName}</h4>
-            <div className={`product-info__price ${props.classNamePrice}`}>
-              <p>
-                {props.oldPriceText} <span className={`product-info__price--old ${props.classNameOldPrice}`}>{props.oldPriceValue}</span>
-              </p>
-              <p>
-                Por <span className={`product-info__price--new ${props.classNameNewPrice}`}>{props.priceValue}</span>
-              </p>
-              <p className="product-info__price--bigger--installments">
-                {props.installmentsText} <span>{props.installments}</span>
-              </p>
-            </div>
-          </div>
+          <ProductInfo
+            classNamePriceGroup="product-info__price-group--search"
+            productName={props.name}
+            oldPriceText="De"
+            oldPriceValue={props.regular_price}
+            priceValue={props.actual_price}
+          />
         </a>
       </li>
     </>
