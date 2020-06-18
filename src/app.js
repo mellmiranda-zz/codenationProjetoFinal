f1007b47630c9fdf9e7c2031260797f31eb08195
NayamaRosa <falecom@nayama.com.br>
2020-06-18 11:58:42 -0300

    componente banner html, js e scss

--- src/app.js
+++ src/app.js
@@ -2,6 +2,16 @@ import React from 'react'
 
 import '@fashionista/css/index.css'
 
+import Banner from 'components/Banner'
+
 export default function App () {
-  return <h1>Hello React App</h1>
+  return (
+    <>
+      <Banner
+        classNameBanner='banner__hero'
+        src='https://viniciusvinna.netlify.app/assets/api-fashionista/20002605_615_catalog_1.jpg'
+        alt='Hero Banner'
+      />
+    </>
+  )
 }
